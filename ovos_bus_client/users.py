b193ec383a032abbdf86b26804b2b8f3cc879493
jarbasai <jarbasai@mailfence.com>
2023-02-02 22:14:46 +0000

    add_location

--- ovos_bus_client/users.py
+++ ovos_bus_client/users.py
@@ -4,10 +4,12 @@ from threading import Lock
 from uuid import uuid4
 
 from ovos_config.config import Configuration
+from ovos_config.locale import get_default_tz
 from ovos_utils.log import LOG
 
 from ovos_bus_client.message import dig_for_message, Message
 from ovos_bus_client.session import SessionManager, Session
+from ovos_backend_client.api import GeolocationApi
 
 
 def _config2preferences(cfg=None):
@@ -17,6 +19,133 @@ def _config2preferences(cfg=None):
     return cfg or {}
 
 
+def _config2location(cfg=None):
+    loc = cfg or Configuration.get()["location"]
+    if isinstance(loc, dict):
+        loc = Location.from_dict(cfg)
+    assert isinstance(loc, Location)
+    return loc
+
+
+class Location:
+    geolocation = GeolocationApi()
+
+    def __init__(self, lat, lon):
+        self.latitude = lat
+        self.longitude = lon
+        # everything is automatically derived from lat/lon
+        # this ensures location is not ambiguous
+        self._address = None
+        self._timezone_code = None
+        self._city = None
+        self._state = None
+        self._country = None
+        self._country_code = None
+
+    @property
+    def timezone_code(self):
+        if self._timezone_code is None:
+            self._timezone_code = get_timezone(self.latitude, self.longitude)
+        return self._timezone_code
+
+    @property
+    def timezone(self):
+        if self.timezone_code:
+            return gettz(self.timezone_code)
+        return get_default_tz()
+
+    @property
+    def current_time(self):
+        return datetime.datetime.now(self.timezone)
+
+    @property
+    def address(self):
+        if not self._address:
+            if not self._city or not self._country:
+                self._reverse_geolocate()
+            self._address = self._city
+            if self._state:
+                self._address += ", " + self._state
+            if self._country:
+                self._address += ", " + self._country
+        return self._address
+
+    @property
+    def city(self):
+        if not self._city:
+            self._reverse_geolocate()
+        return self._city
+
+    @property
+    def state(self):
+        if not self._state:
+            self._reverse_geolocate()
+        return self._state
+
+    @property
+    def country(self):
+        if not self._country:
+            self._reverse_geolocate()
+        return self._country
+
+    @property
+    def country_code(self):
+        if not self._country_code:
+            self._reverse_geolocate()
+        return self._country_code
+
+    @classmethod
+    def _reverse_geolocate(cls):
+        data = cls.geolocation.get_reverse_geolocation(lat, lon)
+        loc = cls.from_dict(data)
+        return loc
+
+    @classmethod
+    def from_address(cls, address):
+        data = cls.geolocation.get_geolocation(address) or {}
+        data["address"] = address
+        location = cls.from_dict(data)
+        return location
+
+    @classmethod
+    def from_dict(cls, data):
+        if isinstance(data, str):
+            data = json.loads(data)
+
+        latitude = data["coordinate"]["latitude"]
+        longitude = data["coordinate"]["longitude"]
+        loc = Location(latitude, longitude)
+        # avoid geolocation api calls if possible
+        loc._city = data["city"]["name"]
+        loc._state = data["city"]["state"].get("name")
+        loc._country = data["city"]["state"]["country"]["name"]
+        loc._timezone_code = data.get("timezone", {}).get("name")
+        loc._address = data.get("address")
+        return loc
+
+    def as_dict(self):
+        return {
+            "city": {
+                "name": self.city,
+                "state": {
+                    "name": self.state,
+                    "country": {
+                        "code": self.country_code,
+                        "name": self.country
+                    }
+                }
+            },
+            "coordinate": {
+                "latitude": self.latitude,
+                "longitude": self.longitude
+            },
+            "timezone": {
+                "name": self.timezone_code
+            },
+            "address": self.address
+        }
+
+
 class UserData:
     """
     An class representing a user, this session data comes in message.context
@@ -24,16 +153,19 @@ class UserData:
     (eg, speaker recognition) or from clients (eg, a chat application)
     """
 
-    def __init__(self, user_id=None, session=None, preferences=None):
+    def __init__(self, user_id=None, session=None,
+                 preferences=None, location=None):
         self.user_id = user_id or str(uuid4())
         self.session = session or SessionManager.get()
         self.preferences = _config2preferences(preferences)
+        self.location = _config2location(location)
 
     def as_dict(self):
         return {
             "user_id": self.user_id,
             "session": self.session.as_dict(),
-            "preferences": self.preferences
+            "preferences": self.preferences,
+            "location": self.location
         }
 
     @staticmethod
@@ -45,18 +177,22 @@ class UserData:
             sess["session_id"] = sid
         sess = Session.from_dict(sess)
         prefs = data.get("preferences")
-        return UserData(uid, session=sess, preferences=prefs)
+        location = data.get("location")
+        return UserData(uid, session=sess,
+                        preferences=prefs, location=location)
 
     @staticmethod
     def from_message(message=None):
         message = message or dig_for_message()
         sess = SessionManager.get(message)
-        uid = prefs = None
+        uid = prefs = location = None
         if message:
             data = message.context.get("user") or {}
             uid = data.get("user_id") or message.context.get("user_id")
             prefs = data.get("preferences") or message.context.get("config")
-        return UserData(uid, session=sess, preferences=prefs)
+            location = data.get("location")
+        return UserData(uid, session=sess,
+                        preferences=prefs, location=location)
 
 
 class UserManager:
