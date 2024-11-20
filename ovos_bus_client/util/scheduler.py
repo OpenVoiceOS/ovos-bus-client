ca138a21c8f9ef694d5051e558ab184afcdd2733
JarbasAI <33701864+JarbasAl@users.noreply.github.com>
2024-11-20 20:15:18 +0000

    Update scheduler.py

--- ovos_bus_client/util/scheduler.py
+++ ovos_bus_client/util/scheduler.py
@@ -31,8 +31,7 @@ from threading import Thread, Lock
 from ovos_config.config import Configuration
 from ovos_config.locations import get_xdg_data_save_path, get_xdg_config_save_path
 from ovos_utils.log import LOG
-from ovos_utils.events import EventContainer
-from ovos_utils.events import EventSchedulerInterface
+from ovos_utils.events import EventContainer, EventSchedulerInterface
 from ovos_bus_client.message import Message
 
 
